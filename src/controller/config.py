fc2313b0e371439dbd77276fb44a27a1929c7a9d
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-11-26 18:16:42 +0100

    Implement config, deps, validation modules

---
+++ src/controller/config.py
@@ -0,0 +1,63 @@
+"""
+Created on Nov 25, 2014
+
+@author: Zsolt Kovari
+
+The module defines Configuration and Repository classes.
+"""
+
+class Configuration:
+    """
+    The class represents a configuration.
+    To add a new Repository object to self.repositories, call add_repository()
+    method instead of self.repositories.append().
+    """
+    all_repositories = list()
+    def __init__(self, scenarios, format, tool, sizes, queries, path,\
+                 maven_xmx, maven_maxpermsize, java_xmx, java_maxpermsize):
+        self.scenarios = scenarios
+        self.format = format
+        self.tool = tool
+        self.sizes = sizes
+        self.queries = queries
+        self.path = path
+        self.maven_xmx = maven_xmx 
+        self.maven_maxpermsize = maven_maxpermsize
+        self.java_xmx = java_xmx
+        self.java_maxpermsize = java_maxpermsize
+        self.repositories = list()
+    
+    
+    def add_repository(self, repository):
+        """
+        Set the configuration repository which represents the tool's location.
+        
+        Parameters:
+        repository: a Repository object
+        """
+        repository.path = self.path
+        self.repositories.append(repository)
+        if (repository in self.all_repositories):
+            self.all_repositories.remove(repository)
+        self.all_repositories.append(repository)
+
+
+class Repository:
+    """The class represents a Git repository.
+    """
+    def __init__(self, name, url, folder, branch, depth):
+        self.name = name
+        self.url = url
+        self.folder = folder
+        self.branch = branch
+        self.depth = depth
+        # initialized when a Repository attached to a Configuration object
+        self.path = None
+        
+    
+    def __eq__(self, other):
+        if (self.name == other.name and self.path == other.path):
+            return True
+        else:
+            return False
+   
