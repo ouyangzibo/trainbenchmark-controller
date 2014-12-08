0b7e38ef86517623b3082261397f870a2faa51fb
ZsoltKovari <kovari.zsolt91@gmail.com>
2014-12-08 21:43:23 +0100

    Change logging formats

--- src/controller/log.py
+++ src/controller/log.py
@@ -21,22 +21,26 @@ def init_log():
     
     if (os.path.exists("../../log/all/logs.txt") == False):
         open("../../log/all/logs.txt", mode="a").close()
-     
+    
+    # unique logging file
     log_file = "../../log/dist/" + time.strftime("%Y-%m-%d %H:%M:%S") + ".txt"
     log_file = log_file.replace(" ", "_")
     log_file = log_file.replace(":", "_")
     open(log_file, mode="w").close()
-    logging.basicConfig(filename=log_file, format="%(levelname)s: %(message)s",\
-                        level=logging.INFO)
+    logging.basicConfig(filename=log_file, format="[%(levelname)s] " +\
+                        "Module:%(module)s, Message:%(message)s", level=logging.INFO)
     
     console_handler = logging.StreamHandler()
+    # common logging file
     file_handler = logging.FileHandler("../../log/all/logs.txt")
     
     file_handler.setLevel(logging.INFO)
-    console_handler.setLevel(logging.WARNING)
-    c_formatter = logging.Formatter('%(levelname)s - %(message)s')
+    console_handler.setLevel(logging.ERROR)
+    c_formatter = logging.Formatter("[%(levelname)s] Module:%(module)s" + \
+                                    ", Message:%(message)s")
     console_handler.setFormatter(c_formatter)
-    f_formatter = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
+    f_formatter = logging.Formatter("%(asctime)s [%(levelname)s] Module:" + \
+                                    "%(module)s, Message:%(message)s")
     file_handler.setFormatter(f_formatter)
     logging.getLogger().addHandler(console_handler)
     logging.getLogger().addHandler(file_handler)
